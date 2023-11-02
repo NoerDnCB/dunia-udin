463632d29dba0f4d5e0c090264761ed422398bc5
NoerDnCB <altosgaming98@gmail.com>
2023-11-02 16:30:45 +0700

    edit website

--- js/script.js
+++ js/script.js
@@ -1,16 +1,68 @@
-// Toggle class active
+// Toggle class active untuk menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika humberger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
-// klik di luar sidebar menghilangkan sidebar
-const hamburger = document.querySelector("#hamburger-menu");
+// Toggle class active untuk search
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toggle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// klik di luar untuk menghilangkan elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const shp = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+
+  if (!shp.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// Modal Box
+const itemDetaiModal = document.querySelector("#item-detail-modal");
+const itemDetailButton = document.querySelectorAll(".item-detail-button");
+
+itemDetailButton.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetaiModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// Klik untuk tutup modal
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetaiModal.style.display = "none";
+  e.preventDefault();
+};
+
+window.onclick = (e) => {
+  if (e.target === itemDetaiModal) {
+    itemDetaiModal.style.display = "none";
+  }
+};
